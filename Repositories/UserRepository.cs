f824fd8962d3e4e82affd429e56b40a49ed14a3c
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-07 13:49:19 +0100

    feat: create a repository for the user interface
    - create the repository without actually implementing the interface methods

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,61 @@
+using bloggin_plataform_api.Models;
+using Microsoft.EntityFrameworkCore;
+using bloggin_plataform_api.Interfaces;
+
+namespace bloggin_plataform_api.Repositories
+{
+    public class UserRepository(DbContext context) : IUserRepository
+    {
+        private readonly DbContext _context = context;
+
+        public bool Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> AddAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
