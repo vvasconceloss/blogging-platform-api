3050106c8035d813b4ebcf7aab58d0013a86d2fb
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-07 14:06:31 +0100

    refactor: remove redundant methods from the user repository interface

--- Interfaces/IUserRepository.cs
+++ Interfaces/IUserRepository.cs
@@ -4,12 +4,6 @@ namespace bloggin_plataform_api.Interfaces
 {
     public interface IUserRepository
     {
-        bool Add(User user);
-        bool Delete(int id);
-        User GetById(int id);
-        User Update(User user);
-        IEnumerable<User> GetAll();
-
         Task<bool> DeleteAsync(int id);
         Task<User> AddAsync(User user);
         Task<User> UpdateAsync(User user);
