f732086681ae08b836f387455682dd9e2decb9d1
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-07 14:07:47 +0100

    feat: create a DTO for the user model (for external responses)

---
+++ DTOs/User/UserResponseDTO.cs
@@ -0,0 +1,8 @@
+namespace bloggin_plataform_api.DTOs.User
+{
+    public class UserResponseDTO
+    {
+        public required string Username { get; set; }
+        public required string EmailAdress { get; set; }
+    }
+}
