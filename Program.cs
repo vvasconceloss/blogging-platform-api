99173c4c979ec207745ec99098071b47ba5152af
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-07 13:27:37 +0100

    feat: configure the db context with the connection string

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using bloggin_plataform_api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BlogDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version())
+    )
+);
 
 builder.Services.AddOpenApi();
 
