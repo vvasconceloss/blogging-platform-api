3449af3287897f475882198ce1f5e4f698316e9d
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-07 21:25:09 +0100

    feat: create a route to create a new user

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using bloggin_plataform_api.DTOs.User;
 using bloggin_plataform_api.Interfaces;
 
 namespace bloggin_plataform_api.Controllers
@@ -6,6 +7,14 @@ namespace bloggin_plataform_api.Controllers
     [ApiController]
     public class UserController(IUserService service) : ControllerBase
     {
-        private readonly IUserService _userService= service;
+        private readonly IUserService _userService = service;
+
+        [HttpPost]
+        [Route("user/create")]
+        public async Task<ActionResult<UserResponseDTO>> AddUser(UserDTO userDTO)
+        {
+            var user = await _userService.AddAsync(userDTO);
+            return user;
+        }
     }
 }
