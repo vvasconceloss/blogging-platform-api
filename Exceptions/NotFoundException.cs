02ab95d0a50b568b6e03aa8cb40113de410a6640
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-08 09:51:39 +0100

    feat: create custom exceptions (error handling)

---
+++ Exceptions/NotFoundException.cs
@@ -0,0 +1,4 @@
+namespace bloggin_plataform_api.Exceptions
+{
+    public class NotFoundException(string message) : Exception(message) { }
+}
---
+++ Exceptions/ValidationException.cs
@@ -0,0 +1,4 @@
+namespace bloggin_plataform_api.Exceptions
+{
+    public class ValidationException(string message) : Exception(message) { }
+}
