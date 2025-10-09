ac6093c234fea86c6e9f046ff64888c15a61da21
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-10-09 12:24:59 +0100

    refactor: update user response DTO fields
    - add ID field
    - rename email field

--- DTOs/User/UserResponseDTO.cs
+++ DTOs/User/UserResponseDTO.cs
@@ -2,7 +2,8 @@ namespace bloggin_plataform_api.DTOs.User
 {
     public class UserResponseDTO
     {
+        public required int Id { get; set; }
         public required string Username { get; set; }
-        public required string EmailAdress { get; set; }
+        public required string EmailAddress { get; set; }
     }
 }
